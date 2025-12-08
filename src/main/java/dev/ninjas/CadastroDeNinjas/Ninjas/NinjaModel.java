66f5a8e7c7be879b162d986b53e277beda350de5
arthurzvz <arthurzinnns7@gmail.com>
2025-12-08 14:09:01 -0300

    fix: Coloca lombok na classe NinjaModel

--- src/main/java/dev/ninjas/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/ninjas/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,16 +2,22 @@ package dev.ninjas.CadastroDeNinjas.Ninjas;
 
 import dev.ninjas.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    int id;
+    private Long id;
     private String nome;
     private String cla;
     private String rank;
@@ -20,46 +26,4 @@ public class NinjaModel {
     @ManyToOne()
     @JoinColumn(name = "missoes_id")
     private MissoesModel missoes;
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String cla, String rank, String tecnicas) {
-        this.nome = nome;
-        this.cla = cla;
-        this.rank = rank;
-        this.tecnicas = tecnicas;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCla() {
-        return cla;
-    }
-
-    public void setCla(String cla) {
-        this.cla = cla;
-    }
-
-    public String getRank() {
-        return rank;
-    }
-
-    public void setRank(String rank) {
-        this.rank = rank;
-    }
-
-    public String getTecnicas() {
-        return tecnicas;
-    }
-
-    public void setTecnicas(String tecnicas) {
-        this.tecnicas = tecnicas;
-    }
 }
