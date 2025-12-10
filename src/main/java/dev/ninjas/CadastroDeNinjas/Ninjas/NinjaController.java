8897a71d4ec5843d5cb39419e61a54c3f71d6828
arthurzvz <arthurzinnns7@gmail.com>
2025-12-10 20:12:39 -0300

    feat: cria os endpoints da classe ninja

--- src/main/java/dev/ninjas/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/ninjas/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.ninjas.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,6 +9,29 @@ public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Minha primeira mensagem com spring";
-
+    }
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Criar ninja";
+    }
+    @GetMapping("/mostrar")
+    public String mostrarNinjaID(){
+        return "mostrar ninja";
+    }
+    @GetMapping("/mostrarNinjas")
+    public String mostrarTodosNinjas(){
+        return "Mostrar todos ninjas";
+    }
+    @GetMapping("/mostrarNinja")
+    public String mostrarTodosNinja(){
+        return "Mostrar todos ninja";
+    }
+    @PutMapping("/alterarNinjaId")
+    public String alterarNinjaId(){
+        return "alterar id do Ninja";
+    }
+    @DeleteMapping("/deletarrNinjaId")
+    public String deletarNinjaId(){
+        return "Deletar Ninja por id";
     }
 }
