9de5668822364c1094794f07a60944b018e8085f
arthurzvz <arthurzinnns7@gmail.com>
2025-12-12 23:38:52 -0300

    feat: adiciona o mapeamento da entidade NinjaModel para NinjaDTO

--- src/main/java/dev/ninjas/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/ninjas/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,17 @@
 package dev.ninjas.CadastroDeNinjas.Ninjas;
+import dev.ninjas.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaDTO {
+    private Long id;
+    private String nome;
+    private String cla;
+    private String email;
+    private MissoesModel missoes;
+    private String rank;
 }
