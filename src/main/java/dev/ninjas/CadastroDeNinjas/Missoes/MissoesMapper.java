21cbdd7b9b523b4ffb278c5d9629d44536555451
arthurzvz <arthurzinnns7@gmail.com>
2025-12-18 23:33:05 -0300

    feat: cria os metodos para mapearem o meu Model para DTO

--- src/main/java/dev/ninjas/CadastroDeNinjas/Missoes/MissoesMapper.java
+++ src/main/java/dev/ninjas/CadastroDeNinjas/Missoes/MissoesMapper.java
@@ -1,4 +1,23 @@
 package dev.ninjas.CadastroDeNinjas.Missoes;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MissoesMapper {
+    public MissoesModel map(MissoesDTO missoesDTO){
+        MissoesModel missoesModel = new MissoesModel();
+        missoesModel.setId(missoesDTO.getId());
+        missoesModel.setNome(missoesDTO.getNome());
+        missoesModel.setNinja(missoesDTO.getNinja());
+        missoesModel.setDificuldade(missoesDTO.getDificuldade());
+        return missoesModel;
+    }
+    public MissoesDTO map(MissoesModel missoesModel){
+        MissoesDTO missoesDTO = new MissoesDTO();
+        missoesDTO.setId(missoesModel.getId());
+        missoesDTO.setNome(missoesDTO.getNome());
+        missoesDTO.setNinja(missoesModel.getNinja());
+        missoesDTO.setDificuldade(missoesModel.getDificuldade());
+        return missoesDTO;
+    }
 }
