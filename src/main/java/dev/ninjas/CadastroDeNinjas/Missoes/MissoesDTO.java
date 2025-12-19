a293beb17977084e3b86c8baf68f04c05a3656dc
arthurzvz <arthurzinnns7@gmail.com>
2025-12-18 23:30:48 -0300

    feat: Adiciona a camada de DTO para o meu MissoesModel

---
+++ src/main/java/dev/ninjas/CadastroDeNinjas/Missoes/MissoesDTO.java
@@ -0,0 +1,18 @@
+package dev.ninjas.CadastroDeNinjas.Missoes;
+
+import dev.ninjas.CadastroDeNinjas.Ninjas.NinjaModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MissoesDTO {
+    private Long id;
+    private String nome;
+    private String dificuldade;
+    private List<NinjaModel> ninja;
+}
