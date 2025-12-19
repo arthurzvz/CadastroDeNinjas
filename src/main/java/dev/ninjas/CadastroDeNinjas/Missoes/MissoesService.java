fbbcdf47ca629c4e6ccf445daa72b6aeda50a7cc
arthurzvz <arthurzinnns7@gmail.com>
2025-12-19 13:33:15 -0300

    feat: cria logica no meu MissoesService para a rota de criar missao

--- src/main/java/dev/ninjas/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/dev/ninjas/CadastroDeNinjas/Missoes/MissoesService.java
@@ -1,4 +1,22 @@
 package dev.ninjas.CadastroDeNinjas.Missoes;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class MissoesService {
+
+    private MissoesRepository missoesRepository;
+    private MissoesMapper missoesMapper;
+
+    public MissoesService(MissoesRepository missoesRepository, MissoesMapper missoesMapper) {
+        this.missoesRepository = missoesRepository;
+        this.missoesMapper = missoesMapper;
+    }
+
+    public MissoesDTO criarMissao(MissoesDTO missoesDTO){
+            MissoesModel missao = missoesMapper.map(missoesDTO); //DTO convertido para Model
+            missoesRepository.save(missao);
+            return missoesMapper.map(missao); //Model convertido para DTO
+    }
+
 }
